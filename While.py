0b93ab78903b404c0ae0d76352ad5e29da6ec5b7
Dhanush V <52505100+Dhanush2612@users.noreply.github.com>
2024-09-27 18:24:20 +0530

    While

---
+++ While.py
@@ -0,0 +1,30 @@
+x=2
+while x<=20:
+    print(x)
+    x=x+2
+print('end loop')
+
+y=1
+while y<=3125:
+    print(y)
+    y=y*5
+print('end loop')
+
+z=128
+while z>=1:
+    print(z)
+    z=z//4
+print('end loop')
+
+a=36
+while a>=1:
+    print(a)
+    a=a-6
+print('end loop')
+
+a=36
+while a>=1:
+    a=a-6
+    print(a)
+print('end loop')
+
